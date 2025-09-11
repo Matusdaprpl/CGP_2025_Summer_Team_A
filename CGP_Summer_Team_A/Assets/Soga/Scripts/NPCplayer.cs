7b4b4c60601111293d729ed28a20ffcff2cde8f2
Soga-CGP <s2510120.soga.cgp@gmail.com>
2025-09-11 22:16:54 +0900

    ?

--- CGP_Summer_Team_A/Assets/Soga/Scripts/NPCplayer.cs
+++ CGP_Summer_Team_A/Assets/Soga/Scripts/NPCplayer.cs
@@ -1,16 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NPCplayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<Tile> hand = new List<Tile>();
+
+    public void Draw(Tile tile)
     {
-        
+        hand.Add(tile);
+        SortHand();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Tile Discard()
     {
-        
+        Tile discarded = hand[hand.Count - 1];
+        hand.RemoveAt(hand.Count - 1);
+        return discarded;
+    }
+
+    void SortHand()
+    {
+        hand = hand.OrderBy(tile => tile.suit)
+                   .ThenBy(tile => tile.rank)
+                   .ToList();
     }
 }
