fa66e0dfd9ce84c6d1178a4d77e398b8c39f4464
Yoshi16121 <s2510316.yoshimasu.cgp@gmail.com>
2025-09-29 04:52:26 +0900

    タイトル画面とリザルト画面

---
+++ CGP_Summer_Team_A/Assets/Yoshimasu/Scripts/taitorumodoru.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class taitorumodoru : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnClick()
+    {
+        SceneManager.LoadScene("taitoru");
+    }
+}
+    
